1784d63cc21f4fb862d17c176fe6ce77e1eba489
Arshia <arshiamotaghian@yahoo.com>
2024-09-19 15:48:27 +0330

    Add holiday

--- auction/user/views.py
+++ auction/user/views.py
@@ -3,19 +3,33 @@ from .serializer import *
 from rest_framework.views import APIView
 import datetime
 from django.http.response import JsonResponse
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView , ListAPIView
+from rest_framework.generics import (
+    ListCreateAPIView,
+    RetrieveUpdateDestroyAPIView,
+    ListAPIView,
+)
 import json
 from .permissions import *
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from datetime import timedelta
-
+import requests
+from datetime import jdatetime
+from datetime import date
 
 
 class Login(TokenObtainPairView):
     pass
 
+
 class Refresh(TokenObtainPairView):
     pass
+
+
+def day_of_week(year, month, day):
+    dates = year + "/" + month + "/" + day
+    url = "https://holidayapi.ir/jalali/" + dates
+    res = requests.get(url)
+    return JsonResponse(json.loads(res.content)["is_holiday"], safe=False)
 
 
 class CheckOffer(APIView):
@@ -24,14 +38,16 @@ class CheckOffer(APIView):
         product_id = data["product_id"]
         customer_id = data["customer_id"]
         price = data["price"]
-        finded_customer = Customer.objects.get(id = customer_id)
+        finded_customer = Customer.objects.get(id=customer_id)
         finded_product = Product.objects.get(id=product_id)
         naive_start_time = finded_product.start_time.replace(tzinfo=None)
         naive_end_time = finded_product.end_time.replace(tzinfo=None)
+        dates = str(date.today()).split("-")
         if (
             naive_start_time <= datetime.datetime.now()
             and naive_end_time >= datetime.datetime.now()
             and price > finded_product.highest_price
+            and day_of_week(dates[0], dates[1], dates[2])
         ):
             Offer.objects.create(
                 product=finded_product,
@@ -53,13 +69,13 @@ class CreateAuction(ListCreateAPIView):
     queryset = Auctions.objects.all()
     serializer_class = AuctionsSerilizer
     permission_classes = [IsProvider]
-    
+
 
 class CreateProvider(ListCreateAPIView):
     queryset = Provider.objects.all()
     serializer_class = ProviderSerilizer
-    
-    
+
+
 class DeleteAuction(RetrieveUpdateDestroyAPIView):
     queryset = Auctions.objects.all()
     serializer_class = AuctionsSerilizer
@@ -71,15 +87,18 @@ class FinishedProducts(APIView):
         finished_products = Product.objects.filter(end_time__lt=datetime.now())
         finished_products = json.dumps(finished_products)
         return JsonResponse(finished_products, safe=False)
-    
+
+
 class CreateProduct(ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerilizer
     permission_classes = [IsProvider]
-    
+
+
 class CreateCustomer(ListCreateAPIView):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerilizer
+
 
 class DetailOffer(ListAPIView):
     queryset = Offer.objects.all()
