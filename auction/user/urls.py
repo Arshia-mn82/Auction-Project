5f9c3e7c4a5c3d3a68832ad6630d2556fd9d08eb
Arshia <arshiamotaghian@yahoo.com>
2024-09-19 13:00:45 +0330

    Last Changes

--- auction/user/urls.py
+++ auction/user/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from .views import *
+
 urlpatterns = [
-    path('create-auction/', CreateAuction.as_view()),
-    path('delete-auction/' , DeleteAuction.as_view()),
-    path('finished-products/' , FinishedProducts.as_view()),
-    
+    path("create-auction/", CreateAuction.as_view()),
+    path("delete-auction/", DeleteAuction.as_view()),
+    path("finished-products/", FinishedProducts.as_view()),
+    path("add-offer/", CheckOffer.as_view()),
+    path('login/' , Login.as_view()),
+    path('refresh/' , Refresh.as_view()),
+    path('create-provider/' , CreateProvider.as_view()),
+    path('create-product/' , CreateProduct.as_view()),
+    path('create-customer/' , CreateCustomer.as_view())
 ]
--- auction/user/views.py
+++ auction/user/views.py
@@ -1,40 +1,42 @@
 from .models import *
 from .serializer import *
 from rest_framework.views import APIView
-from datetime import datetime
+import datetime
 from django.http.response import JsonResponse
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 import json
 from .permissions import *
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from datetime import timedelta
 
 
 class Login(TokenObtainPairView):
     pass
 
-class refresh(TokenObtainPairView):
+class Refresh(TokenObtainPairView):
     pass
 
 
 class CheckOffer(APIView):
-    def post(request, self):
-        data = request.data
-        product_id = data.get("product_id")
-        customer_id = data.get("customer_id")
-        price = data.get("price")
-
+    def post(self, request):
+        data = json.loads(request.body)
+        product_id = data["product_id"]
+        customer_id = data["customer_id"]
+        price = data["price"]
+        finded_customer = Customer.objects.get(id = customer_id)
         finded_product = Product.objects.get(id=product_id)
-
+        naive_start_time = finded_product.start_time.replace(tzinfo=None)
+        naive_end_time = finded_product.end_time.replace(tzinfo=None)
         if (
-            finded_product.start_time <= datetime.now()
-            and finded_product.end_time >= datetime.now()
+            naive_start_time <= datetime.datetime.now()
+            and naive_end_time >= datetime.datetime.now()
             and price > finded_product.highest_price
         ):
             Offer.objects.create(
-                product=product_id,
-                customer=customer_id,
+                product=finded_product,
+                customer=finded_customer,
                 price=price,
-                date=datetime.now(),
+                date=datetime.datetime.now(),
             )
             time_change = datetime.timedelta(minutes=10)
             finded_product.end_time = finded_product.end_time + time_change
@@ -52,7 +54,11 @@ class CreateAuction(ListCreateAPIView):
     permission_classes = [IsProvider]
     
 
-
+class CreateProvider(ListCreateAPIView):
+    queryset = Provider.objects.all()
+    serializer_class = ProviderSerilizer
+    
+    
 class DeleteAuction(RetrieveUpdateDestroyAPIView):
     queryset = Auctions.objects.all()
     serializer_class = AuctionsSerilizer
@@ -65,4 +71,12 @@ class FinishedProducts(APIView):
         finished_products = json.dumps(finished_products)
         return JsonResponse(finished_products, safe=False)
     
+class CreateProduct(ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerilizer
+    permission_classes = [IsProvider]
+    
+class CreateCustomer(ListCreateAPIView):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerilizer
 
